44fa8e577ecd1150c3c0d4de4290a328bb19a66c
Htet Lin Maung <jsthtet96@gmail.com>
2020-12-05 14:58:42 +0630

    error solved

--- dist/graphql/schema.js
+++ dist/graphql/schema.js
@@ -4,8 +4,5 @@ var graphql_1 = require("graphql");
 var graphql_tools_1 = require("../utils/graphql-tools");
 exports.default = graphql_1.buildSchema(graphql_tools_1.mergeSchema([
     graphql_tools_1.importSchema("schemas/authSchema.gql"),
-    graphql_tools_1.importSchema("schemas/categorySchema.gql"),
-    graphql_tools_1.importSchema("schemas/categoryTypeSchema.gql"),
-    graphql_tools_1.importSchema("schemas/menuSchema.gql"),
 ]));
 //# sourceMappingURL=schema.js.map
